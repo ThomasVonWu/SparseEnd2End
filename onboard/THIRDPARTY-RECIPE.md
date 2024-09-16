bc77960029d6063f4aec6d42e6f951ebe08c5c23
ThomasVonWu <romantic.wu@outlook.com>
2024-09-16 23:47:10 +0800

    update glog cmakelists txt

---
+++ onboard/THIRDPARTY-RECIPE.md
@@ -0,0 +1,41 @@
+# ThirdParty Recipe
+
+## Add Submodule:googletest
+```bash
+cd path/to/SparseEnd2End
+git submodule add https://github.com/google/googletest.git onboard/third_party/googletest
+git commit - m "add submodule googletest"
+cd  third_party/googletest
+cmake -B build -S .
+cmake --build build
+```
+<center>
+    <img style="border-radius: 0.3125em;
+    box-shadow: 0 2px 4px 0 rgba(34,36,38,.12),0 2px 10px 0 rgba(34,36,38,.08);" 
+    src="resources/images/gtest.jpg" width="620">
+    <br>
+    <div style="color:orange; border-bottom: 1px solid #d9d9d9;
+    display: inline-block;
+    color: #999;
+    padding: 1px;">If you make successfully, you will get log like this.</div>
+</center>
+
+## Add Submodule:glog
+```bash
+cd path/to/SparseEnd2End
+git submodule add https://github.com/google/glog.git onboard/third_party/glog
+git commit - m "add submodule glog"
+cd  third_party/glog
+cmake -B build -S .
+cmake --build build
+```
+<center>
+    <img style="border-radius: 0.3125em;
+    box-shadow: 0 2px 4px 0 rgba(34,36,38,.12),0 2px 10px 0 rgba(34,36,38,.08);" 
+    src="resources/images/glog.jpg" width="800">
+    <br>
+    <div style="color:orange; border-bottom: 1px solid #d9d9d9;
+    display: inline-block;
+    color: #999;
+    padding: 1px;">If you make successfully, you will get log like this.</div>
+</center>
---
+++ onboard/common/common.h
@@ -0,0 +1,66 @@
+// Copyright (c) 2024 SparseEnd2End. All rights reserved @author: Thomas Von Wu.
+#ifndef ONBOARD_COMMON_COMMON_H
+#define ONBOARD_COMMON_COMMON_H
+
+#include <array>
+#include <vector>
+
+namespace SparseEnd2End {
+namespace image {
+
+static constexpr const char* const kLogContext = "SparseE2ELog";
+
+struct E2EEngine {
+  std::string engine_path;
+  std::vector<std::string> input_names;
+  std::vector<std::string> output_names;
+};
+
+enum class ObstacleType : std::uint8_t {
+  CAR = 0U,
+  TRCUK = 1U,
+  CONSTRUCTION_VEHICLE = 2U,
+  BUS = 3U,
+  TRAILER = 4U,
+  BARRIER = 5U,
+  MOTORCYCLE = 6U,
+  BICYCLE = 7U,
+  PEDESTRIAN = 8U,
+  TRAFFIC_CONE = 9U,
+  OBSTACLETYPE_Max = 10U,
+  OBSTACLETYPE_INVALID = 255U,
+};
+
+struct Obstacle {
+  float x = 0.0F;
+  float y = 0.0F;
+  float z = 0.0F;
+  float w = 0.0F;
+  float l = 0.0F;
+  float h = 0.0F;
+  float yaw = 0.0F;
+  float vx = 0.0F;
+  float vy = 0.0F;
+  float vz = 0.0F;
+  float confidence = 0.0F;
+  float fusioned_confidence = 0.0F;
+  std::uint8_t label = 255U;
+  std::int32_t trackid = -1;
+};
+
+enum class Status : std::uint8_t {
+  kSuccess = 0U,
+
+  kImgPreprocesSizeErr = 1U,
+
+  kBackboneInferErr = 20U,
+
+  kHead1stInferErr = 30U,
+  kHead2ndInferErr = 31U,
+
+  kDecoderErr = 50U
+};
+
+}  // namespace image
+}  // namespace SparseEnd2End
+#endif  // ONBOARD_COMMON_COMMON_H
